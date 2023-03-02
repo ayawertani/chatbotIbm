25f56d4e895aaee560dc55a18ffc8d39af71a31a
mo5tarz <zghalmokhtar@yahoo.com>
2023-03-02 18:59:47 +0100

    add post request of dict and convenient questions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 import {HttpClient  } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'Basic ' + btoa('apikey:gonGjOBnw_kd-DCPmduSpvPCzCInN-SM3W_u4H4lJTgn')
+  })
+};
+const x='{"intent":"'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,9 +18,16 @@ export class AppComponent {
   constructor(private http:HttpClient){}
   d=[this.http.get('http://localhost:1000/diconnary')] ;
   title = 'PFA';
-  onSubmit(data:any)
-  { this.http.post('http://localhost:1000/diconnary/....',data)
-  .subscribe(()=>console.warn)
-    console.warn(data);
+  onSubmit(data:string)
+  { console.log(data);
+     this.http.post('https://api.au-syd.assistant.watson.cloud.ibm.com/instances/18b8007d-97e0-478d-9f54-27cc3bec8c2c/v1/workspaces/3756dbf5-ea5c-43cf-a0d2-81dfa1bbe60b/intents/hi?version=2023-02-01'
+  ,this.jsonIntent(JSON.parse(data.toString().replace("'", "\"")).dictionnary,JSON.parse(data).question), httpOptions).subscribe((result: any) => {
+    console.log(result);
+  }, (error: any) => console.log('There was an error: '));
+}
+  
+ jsonIntent(dict:string,userInput:string):string {
+    return '{"intent":"'+dict+'","examples":[{"text":"'+userInput+'"}]}'
+    
   }
 }
